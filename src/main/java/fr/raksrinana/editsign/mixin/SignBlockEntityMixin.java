ca1d4c6184a1378412b0e2585506b8606bf5c3f8
Thomas Couchoud <thomas.couchoud@gmail.com>
2020-08-09 20:06:37 +0200

    Fixes backport issues

--- src/main/java/fr/raksrinana/editsign/mixin/SignBlockEntityMixin.java
+++ src/main/java/fr/raksrinana/editsign/mixin/SignBlockEntityMixin.java
@@ -3,7 +3,6 @@ package fr.raksrinana.editsign.mixin;
 import net.minecraft.block.entity.SignBlockEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.text.TranslatableText;
-import net.minecraft.util.Util;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -19,7 +18,7 @@ public final class SignBlockEntityMixin{
 			player.openEditSignScreen(sign);
 		}
 		else{
-			player.sendSystemMessage(new TranslatableText("edit_sign.action.not_editable"), Util.NIL_UUID);
+			player.sendMessage(new TranslatableText("edit_sign.action.not_editable"));
 		}
 	}
 }
