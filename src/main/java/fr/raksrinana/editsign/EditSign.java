657eeb78982ad494c65f0fe37a49fb93116c11b9
Thomas Couchoud <thomas.couchoud@gmail.com>
2020-08-09 16:38:18 +0200

    Experimental fabric version

--- src/main/java/fr/raksrinana/editsign/EditSign.java
+++ src/main/java/fr/raksrinana/editsign/EditSign.java
@@ -1,36 +1,19 @@
 package fr.raksrinana.editsign;
 
-import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.fml.ModContainer;
-import net.minecraftforge.fml.ModList;
-import net.minecraftforge.fml.common.Mod;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.ModInitializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import java.util.Optional;
 
-@Mod(EditSign.MOD_ID)
-public class EditSign{
+public class EditSign implements ClientModInitializer{
 	public static final String MOD_ID = "edit_sign";
-	public static final String MOD_NAME = "Edit Sign";
-	public static final String VERSION = "2.0.0";
 	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 	
 	public EditSign(){
 	}
 	
-	public static String getVersion(){
-		Optional<? extends ModContainer> o = ModList.get().getModContainerById(MOD_ID);
-		if(o.isPresent()){
-			return o.get().getModInfo().getVersion().toString();
-		}
-		return "NONE";
-	}
+	@Override
+	public void onInitializeClient(){
 	
-	public static boolean isDevBuild(){
-		return "NONE".equals(getVersion());
-	}
-	
-	public static ResourceLocation getId(String path){
-		return new ResourceLocation(MOD_ID, path);
 	}
 }
