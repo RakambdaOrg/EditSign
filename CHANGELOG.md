10fb1e6b2eabe1a61da3ca93033bbe8b55f084c6
Thomas Couchoud <thomas.couchoud@gmail.com>
2020-08-13 18:42:03 +0200

    Fix #7: Load on server

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+## [Fabric-1.16.1-2.1.1] - 2020-08-13
+* Load mod on servers (#7)
+
 ## [Fabric-1.16.1-2.1.0] - 2020-08-12
 * Only edit sign in survival or creative (#6)
 
--- src/main/java/fr/raksrinana/editsign/mixin/SignBlockEntityMixin.java
+++ src/main/java/fr/raksrinana/editsign/mixin/SignBlockEntityMixin.java
@@ -2,32 +2,23 @@ package fr.raksrinana.editsign.mixin;
 
 import net.minecraft.block.entity.SignBlockEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.text.TranslatableText;
-import net.minecraft.util.Util;
-import net.minecraft.world.GameMode;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(SignBlockEntity.class)
 public final class SignBlockEntityMixin{
+	@Shadow
+	private boolean editable;
+	
 	@Inject(method = "onActivate", at = @At("HEAD"))
 	public void useOnBlock(PlayerEntity player, CallbackInfoReturnable<Boolean> callback){
-		if(player instanceof ServerPlayerEntity){
-			ServerPlayerEntity serverPlayerEntity = (ServerPlayerEntity) player;
-			GameMode gameMode = serverPlayerEntity.interactionManager.getGameMode();
-			if(gameMode == GameMode.SURVIVAL || gameMode == GameMode.CREATIVE){
-				SignBlockEntity sign = (SignBlockEntity) (Object) this;
-				sign.setEditable(true);
-				if(sign.isEditable()){
-					player.openEditSignScreen(sign);
-				}
-				else{
-					player.sendSystemMessage(new TranslatableText("edit_sign.action.not_editable"), Util.NIL_UUID);
-				}
-			}
+		if(player.abilities.allowModifyWorld){
+			editable = true;
+			SignBlockEntity sign = (SignBlockEntity) (Object) this;
+			player.openEditSignScreen(sign);
 		}
 	}
 }
