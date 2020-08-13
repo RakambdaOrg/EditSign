ecda4332d6a4e5e9abbdbf1593b57204266c1d3e
Thomas Couchoud <thomas.couchoud@gmail.com>
2020-08-13 17:26:34 +0200

    Check player abilities instead of gamemode

--- src/main/java/fr/raksrinana/editsign/mixin/SignBlockEntityMixin.java
+++ src/main/java/fr/raksrinana/editsign/mixin/SignBlockEntityMixin.java
@@ -2,9 +2,7 @@ package fr.raksrinana.editsign.mixin;
 
 import net.minecraft.block.entity.SignBlockEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.TranslatableText;
-import net.minecraft.world.GameMode;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -13,19 +11,15 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(SignBlockEntity.class)
 public final class SignBlockEntityMixin{
 	@Inject(method = "onActivate", at = @At("HEAD"))
-	public void useOnBlock(PlayerEntity player, CallbackInfoReturnable<Boolean> callback){
-		if(player instanceof ServerPlayerEntity){
-			ServerPlayerEntity serverPlayerEntity = (ServerPlayerEntity) player;
-			GameMode gameMode = serverPlayerEntity.interactionManager.getGameMode();
-			if(gameMode == GameMode.SURVIVAL || gameMode == GameMode.CREATIVE){
-				SignBlockEntity sign = (SignBlockEntity) (Object) this;
-				sign.setEditable(true);
-				if(sign.isEditable()){
-					player.openEditSignScreen(sign);
-				}
-				else{
-					player.sendMessage(new TranslatableText("edit_sign.action.not_editable"));
-				}
+	public void onActivate(PlayerEntity player, CallbackInfoReturnable<Boolean> callback){
+		if(player.abilities.allowModifyWorld){
+			SignBlockEntity sign = (SignBlockEntity) (Object) this;
+			sign.setEditable(true);
+			if(sign.isEditable()){
+				player.openEditSignScreen(sign);
+			}
+			else{
+				player.sendMessage(new TranslatableText("edit_sign.action.not_editable"));
 			}
 		}
 	}
