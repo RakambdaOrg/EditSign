01cbb2a7a623f91bb37d7ce4728de1a40f7f29cf
Thomas Couchoud <thomas.couchoud@gmail.com>
2020-08-13 18:45:57 +0200

    Fix regression where sign isn't editable

--- src/main/java/fr/raksrinana/editsign/mixin/SignBlockEntityMixin.java
+++ src/main/java/fr/raksrinana/editsign/mixin/SignBlockEntityMixin.java
@@ -3,15 +3,20 @@ package fr.raksrinana.editsign.mixin;
 import net.minecraft.block.entity.SignBlockEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(SignBlockEntity.class)
 public final class SignBlockEntityMixin{
+	@Shadow
+	private boolean editable;
+	
 	@Inject(method = "onActivate", at = @At("HEAD"))
 	public void useOnBlock(PlayerEntity player, CallbackInfoReturnable<Boolean> callback){
 		if(player.abilities.allowModifyWorld){
+			editable = true;
 			SignBlockEntity sign = (SignBlockEntity) (Object) this;
 			player.openEditSignScreen(sign);
 		}
