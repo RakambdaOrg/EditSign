bfec4ea4046503ff064519fff53ee5ae98ad91b4
Thomas Couchoud <zerderr@gmail.com>
2023-03-16 17:59:37 +0100

    fix: components

--- forge/src/main/java/fr/rakambda/editsign/forge/config/cloth/ClothConfigHook.java
+++ forge/src/main/java/fr/rakambda/editsign/forge/config/cloth/ClothConfigHook.java
@@ -7,9 +7,6 @@ import me.shedaniel.clothconfig2.api.ConfigBuilder;
 import me.shedaniel.clothconfig2.api.ConfigCategory;
 import me.shedaniel.clothconfig2.gui.entries.StringListEntry;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.MutableComponent;
-import net.minecraft.network.chat.contents.LiteralContents;
-import net.minecraft.network.chat.contents.TranslatableContents;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.ConfigScreenHandler;
@@ -71,7 +68,7 @@ public class ClothConfigHook{
 	
 	@NotNull
 	private static Component translatable(@NotNull String key){
-		return MutableComponent.create(new TranslatableContents(key));
+		return Component.translatable(key);
 	}
 	
 	private String getFieldName(String fieldName){
