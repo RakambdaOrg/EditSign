45ac3ff7145014dc20c4f0dbf29817f6af11e806
Thomas Couchoud <zerderr@gmail.com>
2023-03-16 19:55:14 +0100

    feat: forgot some parts

--- forge/src/main/java/fr/rakambda/editsign/forge/EditSign.java
+++ forge/src/main/java/fr/rakambda/editsign/forge/EditSign.java
@@ -2,16 +2,12 @@ package fr.rakambda.editsign.forge;
 
 import fr.rakambda.editsign.common.EditSignCommon;
 import fr.rakambda.editsign.forge.common.EditSignCommonsImpl;
-import fr.rakambda.editsign.forge.config.Config;
 import fr.rakambda.editsign.forge.config.cloth.ClothConfigHook;
 import lombok.Getter;
 import lombok.extern.log4j.Log4j2;
-import net.minecraftforge.fml.IExtensionPoint.DisplayTest;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.ModList;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.network.NetworkConstants;
 import java.lang.reflect.InvocationTargetException;
 
 @Log4j2
@@ -22,9 +18,6 @@ public class EditSign{
 	private static final EditSignCommonsImpl mod = new EditSignCommonsImpl();
 	
 	public EditSign(){
-		ModLoadingContext.get().registerExtensionPoint(DisplayTest.class, () -> new DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
-		ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.COMMON_SPEC);
-		
 		if(ModList.get().isLoaded("cloth_config")){
 			try{
 				Class.forName("fr.rakambda.fallingtree.forge.client.cloth.ClothConfigHook")
@@ -37,5 +30,7 @@ public class EditSign{
 				log.error("Failed to hook into ClothConfig", e);
 			}
 		}
+		
+		mod.registerForge(MinecraftForge.EVENT_BUS);
 	}
 }
--- forge/src/main/java/fr/rakambda/editsign/forge/common/EditSignCommonsImpl.java
+++ forge/src/main/java/fr/rakambda/editsign/forge/common/EditSignCommonsImpl.java
@@ -5,11 +5,16 @@ import fr.rakambda.editsign.common.wrapper.IComponent;
 import fr.rakambda.editsign.common.wrapper.IItem;
 import fr.rakambda.editsign.forge.common.wrapper.ComponentWrapper;
 import fr.rakambda.editsign.forge.common.wrapper.ItemWrapper;
+import fr.rakambda.editsign.forge.event.RightClickBlockListener;
 import net.minecraft.core.Holder;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.IExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.network.NetworkConstants;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.IForgeRegistry;
 import org.jetbrains.annotations.NotNull;
@@ -26,7 +31,7 @@ public class EditSignCommonsImpl extends EditSignCommon{
 	}
 	
 	@Nonnull
-	public Stream<IItem> getItem(String name){
+	public Stream<IItem> getItem(@NotNull String name){
 		try{
 			var isTag = name.startsWith("#");
 			if(isTag){
@@ -45,12 +50,18 @@ public class EditSignCommonsImpl extends EditSignCommon{
 	}
 	
 	@NotNull
-	private static <T> Optional<T> getRegistryElement(IForgeRegistry<T> registryKey, ResourceLocation identifier){
+	private static <T> Optional<T> getRegistryElement(@NotNull IForgeRegistry<T> registryKey, @NotNull ResourceLocation identifier){
 		return registryKey.getHolder(identifier).map(Holder::value);
 	}
 	
 	@NotNull
 	private static <T> Stream<T> getRegistryTagContent(@NotNull IForgeRegistry<T> registry, @NotNull TagKey<T> tag){
 		return registry.tags().getTag(tag).stream();
+	}
+	
+	public void registerForge(@NotNull IEventBus eventBus){
+		ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+		
+		eventBus.register(new RightClickBlockListener(this));
 	}
 }
